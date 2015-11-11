5819c41f3029cb3ebd52ef2071ce5b4ec18a3076
kageiger <kageiger@umich.edu>
2015-11-10 23:33:25 -0500

    Create Question.cpp

---
+++ Question.cpp
@@ -0,0 +1,28 @@
+/**
+ * Question.cpp
+ *
+ * Katherine Geiger, Keyaria Walker
+ * kageiger, keyariaw
+ *
+ * EECS 183: Project 4
+ *
+ * A test program that matches users based on their answers 
+ * to a personality quiz
+ */
+
+#include "Question.h"
+
+
+// TODO: implement constructor, getValueWithinRange, getNumAnswers,
+//       getQuestionPrompt, setQuestionPrompt, getAnswer, addAnswer,
+//       getAttributeOfAnswer
+
+
+void Question::read(istream &ins) {
+    // TODO: implement
+}
+
+
+void Question::write(ostream &outs) {
+    // TODO: implement
+}
