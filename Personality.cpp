ca9abf9b374411b17000779c15844db372ee6d07
kageiger <kageiger@umich.edu>
2015-11-10 23:32:07 -0500

    Create Personality.cpp

---
+++ Personality.cpp
@@ -0,0 +1,23 @@
+/**
+ * Personality.cpp
+ *
+ * Katherine Geiger, Keyaria Walker
+ * kageiger, keyariaw
+ *
+ * EECS 183: Project 4
+ *
+ * This program matches users based on anwers to a personality quiz
+ */
+
+
+#include <iostream>
+
+#include "Personality.h"
+
+
+// TODO: implement reset, constructor, adjustPersonality
+
+
+void Personality::write(ostream &outs) {
+    // TODO: implement
+}
