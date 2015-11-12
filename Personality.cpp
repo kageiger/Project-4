6383989cf3856a27f19c85686fc20c0ad0bfd374
kageiger <kageiger@umich.edu>
2015-11-12 12:07:53 -0500

    Update Personality.cpp

--- Personality.cpp
+++ Personality.cpp
@@ -14,10 +14,49 @@
 
 #include "Personality.h"
 
+void Personality::reset() {
+    for (int i = 0; i < MAX_NUM_ATTRIBUTES; i++) {
+        points[i] = DEFAULT_POINTS;
+    }
+}
 
-// TODO: implement reset, constructor, adjustPersonality
+Personality::Personality() {
+    reset();
+}
 
+//Changes the value of the personality based on user's answers
+
+void Personality::adjustPersonality(Attribute& inAttribute) {
+
+    Attribute getIndex;
+
+    //  add inAttribute to points
+    //  inAttribute = MAX_NUM_ATTRIBUTES
+    //  if negative points cause < Min_Points set = to Min_Points
+    //  if > MAX_POINTS set = to MAX_POINTs
+
+    points[inAttribute.getIndex()] += inAttribute.getPoints();
+
+    if (points[inAttribute.getIndex()] < MIN_POINTS) {
+
+        points[inAttribute.getIndex()] = MIN_POINTS;
+    }
+    if (points[inAttribute.getIndex()] > MAX_POINTS) {
+
+        points[inAttribute.getIndex()] = MAX_POINTS;
+    }
+
+
+}
+
+
+//personality.p;
+//p.write(cout):
 
 void Personality::write(ostream &outs) {
-    // TODO: implement
+
+    cout << "Your EECS Harmony Personality Profile" << endl;
+    for (int i = 0; i < MAX_NUM_ATTRIBUTES; i++) {
+        outs << ATTRIBUTE_NAMES[i] << ": " << points[i] << endl;
+    }
 }
